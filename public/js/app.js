8bfbe283bfcbcaaac36dd279bb1cc6a9d0641e98
Dheeraj <boat@pupasoft.com>
2014-11-03 21:21:25 +0700

    add table in app.js and show table form app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,8 @@
 angular.module("myApp", [])
 .controller('mainCtrl', function($scope){
-	$scope.name = 'Dream';
+	$scope.books = [
+		{title: 'Ragnarok', price: 200},
+		{title: 'Maple', price: 300},
+		{title: 'Yulgang', price: 400}
+	];
 })
