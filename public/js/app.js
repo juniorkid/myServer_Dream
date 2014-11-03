ba20ea14f9bd52bd17f2a9581c63a8025e48b6c3
Dheeraj <boat@pupasoft.com>
2014-11-03 21:30:28 +0700

    get table form server

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,7 @@
 angular.module("myApp", [])
-.controller('mainCtrl', function($scope){
-	$scope.books = [
-		{title: 'Ragnarok', price: 200},
-		{title: 'Maple', price: 300},
-		{title: 'Yulgang', price: 400}
-	];
+.controller('mainCtrl', function($scope,$http){
+	$scope.books = [];
+	$http.get('/api/book').success(function(data){
+		$scope.books=data;
+	})
 })
